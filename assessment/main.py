a6cf837dbf150f5051729a620295a775c2758fcf
Shaurya Rishi <shaurya.rishi@yahoo.com>
2022-04-19 10:00:03 +0400

    Fixed Merge Error

--- assessment/main.py
+++ assessment/main.py
@@ -1,12 +1,6 @@
 from ciphers.caesar import CaesarCipherInstance
 
 def main():
-	inst1 = CaesarCipherInstance("My name is Shaurya, and I'm 13 years old!", "", 12)
-	inst1.encrypt()
-	print(inst1.ciphertext)
-	inst2 = CaesarCipherInstance("", inst1.ciphertext, inst1.key)
-	inst2.decrypt()
-	print(inst2.plaintext)
-
+	
 if __name__ == "__main__":
 	main()
