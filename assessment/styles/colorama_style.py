7553a91bc0d38c5ca4175a7b7bc4fa252629508a
Shaurya Rishi <shaurya.rishi@yahoo.com>
2022-04-27 08:19:14 +0400

    Reorganised colorama styles

---
+++ assessment/styles/colorama_style.py
@@ -0,0 +1,6 @@
+# Contains colours repeated throughout the program to prevent repetition of Style.STYLE
+from colorama import Style
+
+main_style = Style.BRIGHT
+option_style = Style.BRIGHT + Style.DIM
+reset_style = Style.RESET_ALL
